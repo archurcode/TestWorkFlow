843c02458b84bbf914acb5bed0e92f787a38d100
archurtan <archurspace@gmail.com>
2021-04-21 16:00:14 +0800

    test

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -7,7 +7,11 @@ rc=$(git rev-parse --short HEAD)
 rb=$(git rev-parse --abbrev-ref HEAD)
 rbb=$(git symbolic-ref --short -q HEAD)
 rbbb=$(git branch --contains $rtt | grep '*')
-echo $rc
-echo $rb
-echo $rbb
-echo $rbbb
+rbbbb=$(git branch --contains v1.0.2 | grep '*')
+
+echo rtt=======$rtt
+echo rc=======$rc
+echo rb=======$rb
+echo rbb=======$rbb
+echo rbbb=======$rbbb
+echo rbbbb=======$rbbbb
