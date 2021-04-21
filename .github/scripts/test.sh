0a7bced2a8b1f55a9840ebcf8723b6b887975557
archurtan <archurspace@gmail.com>
2021-04-21 15:47:53 +0800

    test

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+#set -eo pipefail
+set -e
 
-git clone https://github.com/tonychanchen/iot-version.git
-cd iot-version
-ls
-
-echo "版本号---$TEST"
+rtt=$(git describe --tags `git rev-list --tags --max-count=1`)
+rc=$(git rev-parse --short HEAD)
+rb=$(git rev-parse --abbrev-ref HEAD)
+rbb=$(git symbolic-ref --short -q HEAD)
+rbbb=$(git branch --contains $rtt | grep '*')
+echo $rc
+echo $rb
+echo $rbb
+echo $rbbb
