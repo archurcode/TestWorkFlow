310f7effdcdf2d3381373a1f43f2fc1e5d14759e
archurtan <archurspace@gmail.com>
2021-04-22 17:20:23 +0800

    test

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -23,3 +23,15 @@ echo ============2
 git describe --tags `git rev-list --tags --max-count=1`
 echo ============3
 git describe --tags --abbrev=0
+
+
+
+rtt=$(git describe --tags `git rev-list --tags --max-count=1`)
+rttt=$(git describe --tags --abbrev=0)
+rc=$(git rev-parse --short HEAD)
+rb=$(git rev-parse --abbrev-ref HEAD)
+echo 111-$rtt
+echo 111-$rttt
+echo 222-$rc
+echo 333-$rb
+echo 444-$GIT_BRANCH_TAG
