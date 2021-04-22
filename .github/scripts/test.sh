522c4e810cbe085b69218a5ec9f2ed65cc15bd1e
archurtan <archurspace@gmail.com>
2021-04-22 17:15:05 +0800

    test

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -9,9 +9,17 @@ rbb=$(git symbolic-ref --short -q HEAD)
 rbbb=$(git branch --contains $rtt | grep '*')
 rbbbb=$(git branch --contains v1.0.2 | grep '*')
 
-echo rtt=======$rtt
-echo rc=======$rc
-echo rb=======$rb
-echo rbb=======$rbb
-echo rbbb=======$rbbb
-echo rbbbb=======$rbbbb
+# echo rtt=======$rtt
+# echo rc=======$rc
+# echo rb=======$rb
+# echo rbb=======$rbb
+# echo rbbb=======$rbbb
+# echo rbbbb=======$rbbbb
+
+echo ${{ steps.tag.outputs.result }}
+echo ============1
+git tag
+echo ============2
+git describe --tags `git rev-list --tags --max-count=1`
+echo ============3
+git describe --tags --abbrev=0
