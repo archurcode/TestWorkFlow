3844bccb6e8248d15dbf65cbbd596e66c7ef6e16
archurcode <archurspace@gmail.com>
2021-11-15 10:58:59 +0800

    Create send_result_to_wecom.sh

---
+++ .github/scripts/send_result_to_wecom.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=$IOT_WECOM_CID_ROBOT_KEY' \
+   -H 'Content-Type: application/json' \
+   -d '
+   {
+        "msgtype": "text",
+        "text": {
+            "content": "hello world",
+            "mentioned_list":["谭宇超"]  
+        }
+   }'
