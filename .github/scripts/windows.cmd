63a62f962ac1a7a372ee34e21296d5696c988a4a
archurcode <archurspace@gmail.com>
2022-02-17 15:43:03 +0800

    Update windows.cmd

--- .github/scripts/windows.cmd
+++ .github/scripts/windows.cmd
@@ -22,8 +22,10 @@ echo 111---%rtt%
 echo 222---%rc%
 echo 333---%rb%
 
+call echo %%GIT_ACCESS_TOKEN%%
 
-#1.拉取eNet支持库
+echo %%GIT_ACCESS_TOKEN%%
+
 git clone https://$GIT_ACCESS_TOKEN@github.com/tencentyun/iot-p2p.git
 cd iot-p2p
 
