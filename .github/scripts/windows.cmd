d222edc0fa94bab2101b0f9b459a0c2663d4b083
archurcode <archurspace@gmail.com>
2022-02-17 15:07:20 +0800

    Update windows.cmd

--- .github/scripts/windows.cmd
+++ .github/scripts/windows.cmd
@@ -1,10 +1,6 @@
 REM ############################## windows cmd
 REM Test
 
-$branch="${{ github.ref }}"
+$branch=%1
 $branch=$branch.Split("/")[2]
 echo $branch
-$GIT_BRANCH_IMAGE_VERSION=$branch
-echo $GIT_BRANCH_IMAGE_VERSION
-$test=${{ secrets.IOT_WECOM_CID_ROBOT_KEY }}
-echo $test
