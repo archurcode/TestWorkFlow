7d742f9f69edb1bdd6b6ba354d7f7b676b195dd8
archurcode <archurspace@gmail.com>
2022-02-17 15:28:29 +0800

    Update windows.cmd

--- .github/scripts/windows.cmd
+++ .github/scripts/windows.cmd
@@ -3,5 +3,14 @@ REM Test
 
 set branch=%1
 echo %branch%
-echo $Env:IOT_WECOM_CID_ROBOT_KEY
-echo %Env:IOT_WECOM_CID_ROBOT_KEY%
+
+set rtt=%1
+set rc=$(git rev-parse --short HEAD)
+set rb=$(git rev-parse --abbrev-ref HEAD)
+set currtag=$(git describe --tags --match "v[0-9]*" --abbrev=0 HEAD)
+set currbra=%rb%
+echo 000---%currtag%
+echo 111---%rtt%
+echo 222---%rc%
+echo 333---%rb%
+
