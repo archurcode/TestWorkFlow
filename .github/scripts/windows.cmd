ae801b348d56478557c330a9bd77796d45e9d9dd
archurcode <archurspace@gmail.com>
2022-02-17 15:30:19 +0800

    Update windows.cmd

--- .github/scripts/windows.cmd
+++ .github/scripts/windows.cmd
@@ -5,9 +5,9 @@ set branch=%1
 echo %branch%
 
 set rtt=%1
-set rc=$(git rev-parse --short HEAD)
-set rb=$(git rev-parse --abbrev-ref HEAD)
-set currtag=$(git describe --tags --match "v[0-9]*" --abbrev=0 HEAD)
+set rc=`git rev-parse --short HEAD`
+set rb=`git rev-parse --abbrev-ref HEAD`
+set currtag=`git describe --tags --match "v[0-9]*" --abbrev=0 HEAD`
 set currbra=%rb%
 echo 000---%currtag%
 echo 111---%rtt%
